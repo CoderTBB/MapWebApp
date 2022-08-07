1ecfc6a571ccc309775b4e13e4b246fdc428c9c2
谭波波 <bbtan@streamax.com>
2022-08-08 00:08:57 +0800

    添加样式

--- src/views/MapPage.jsx
+++ src/views/MapPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Map, Marker, NavigationControl, InfoWindow } from "react-bmapgl";
-
+import './css/MapPage.css'
 export class MapPage extends Component {
   constructor(props) {
     super(props);
@@ -11,7 +11,7 @@ export class MapPage extends Component {
   componentDidMount() {}
   render() {
     return (
-      <div>
+      <div className="bgView">
         <Map center={{ lng: 116.402544, lat: 39.928216 }} zoom="11">
           <Marker position={{ lng: 116.402544, lat: 39.928216 }} />
           <NavigationControl />
